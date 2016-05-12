1bb17813c6ba7a4d7fb422b0d1d866142ce37781
aguiu <aguiuperez@gmail.com>
2016-05-12 12:56:10 +0200

    Selección de películas en la p. de resultados v2

--- Cine/app/src/main/java/trelligen/app/cine/pantalla/ListViewAdapter.java
+++ Cine/app/src/main/java/trelligen/app/cine/pantalla/ListViewAdapter.java
@@ -1,6 +1,7 @@
 package trelligen.app.cine.pantalla;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,24 +27,25 @@ public class ListViewAdapter extends BaseAdapter {
     Pelicula[][] peliculas; //Arrat de películas.
     LayoutInflater inflater;
 
-	
-	/*
+
+    /*
 	 * Método que obtiene las películas de cada fila.
      */
     public ListViewAdapter(Context context, ArrayList<Pelicula> listaRecibida) {
         this.context = context;
-        peliculas = new Pelicula[(listaRecibida.size()+1)/2][2];
-        int i=0,j=0;	// Índices para recorrer el array.
-        while(listaRecibida.size()>j){	// Recorre la lista de películas y las obtiene.
+        peliculas = new Pelicula[(listaRecibida.size() + 1) / 2][2];
+        int i = 0, j = 0;    // Índices para recorrer el array.
+        while (listaRecibida.size() > j) {    // Recorre la lista de películas y las obtiene.
             peliculas[i][0] = listaRecibida.get(j);
-            if(listaRecibida.size()>j+1) {  //Comprueba que haya una segunda pelicula para esta fila.
+            if (listaRecibida.size() > j + 1) {  //Comprueba que haya una segunda pelicula para esta fila.
                 peliculas[i][1] = listaRecibida.get(j + 1);
             }
-            i++; j+=2;
+            i++;
+            j += 2;
         }
     }
 
-	/*
+    /*
 	 * Método que devuelve el número de filas de películas.
      */
     @Override
@@ -51,7 +53,7 @@ public class ListViewAdapter extends BaseAdapter {
         return peliculas.length;
     }
 
-	/*
+    /*
      * Método que devuelve la película de una posición.
      */
     @Override
@@ -59,7 +61,7 @@ public class ListViewAdapter extends BaseAdapter {
         return null;
     }
 
-	/*
+    /*
      * Método que obtiene el índice del elemento de una posición dada.
      */
     @Override
@@ -67,19 +69,21 @@ public class ListViewAdapter extends BaseAdapter {
         return 0;
     }
 
-	/*
+    /*
      * Método que muestra las películas por pantalla, indicando dos por fila.
      */
-    public View getView(int position, View convertView, ViewGroup parent) {
-
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        /* "position" se declara como "final" para poder usarlo dentro de los
+         *  eventos al seleccionar una película
+         */
         // Variables para las dos películas de cada fila.
-        final TextView txtTitle1,txtTitle2;
-        ImageView imgImg1,imgImg2;
+        final TextView txtTitle1, txtTitle2;
+        ImageView imgImg1, imgImg2;
 
         // Prepara la pantalla para mostrar las películas.
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		// Número de películas a mostrar.
+        // Número de películas a mostrar.
         View itemView = inflater.inflate(R.layout.list_row, parent, false);
 
         // Obtiene los elementos de la pantalla.
@@ -89,56 +93,64 @@ public class ListViewAdapter extends BaseAdapter {
         imgImg2 = (ImageView) itemView.findViewById(R.id.resultado_imagen2);
 
         // Comprueba si existen películas para esa posición y las muestra u oculta.
-        if(peliculas[position][0]==null){
+        if (peliculas[position][0] == null) {
             imgImg1.setVisibility(View.INVISIBLE);
             txtTitle1.setVisibility(View.INVISIBLE);
-        }else {
+        } else {
             txtTitle1.setText(peliculas[position][0].getTitulo());
             imgImg1.setImageBitmap(mostrarImagen(peliculas[position][0].getURL()));
             //Gestiona la pulsación de la imagen del resultado izquierdo
             imgImg1.setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
-                    if(event.getAction()==MotionEvent.ACTION_DOWN) {
-                        Log.d("PeliIzquierda", txtTitle1.getText().toString());
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        // Carga la pantalla de información detallada
+                        Intent i = new Intent(context, InfoPelicula.class);
+                        i.putExtra("pelicula", peliculas[position][0].getId());
+                        context.startActivity(i);
                         return true;
-                    }return false;
+                    }
+                    return false;
                 }
             });
         }
-        if(peliculas[position][1]==null) {
+        if (peliculas[position][1] == null) {
             imgImg2.setVisibility(View.INVISIBLE);
             txtTitle2.setVisibility(View.INVISIBLE);
-        }else {
+        } else {
             txtTitle2.setText(peliculas[position][1].getTitulo());
             imgImg2.setImageBitmap(mostrarImagen(peliculas[position][1].getURL()));
             //Gestiona la pulsación de la imagen del resultado derecho
             imgImg2.setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
-                    if(event.getAction()==MotionEvent.ACTION_DOWN) {
-                        Log.d("PeliDerecha", txtTitle2.getText().toString());
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        // Carga la pantalla de información detallada
+                        Intent i = new Intent(context, InfoPelicula.class);
+                        i.putExtra("pelicula", peliculas[position][1].getId());
+                        context.startActivity(i);
                         return true;
-                    }return false;
+                    }
+                    return false;
                 }
             });
         }
         return itemView;
     }
 
-	/*
+    /*
      * Método que muestra las imágenes por pantalla.
      */
-    private Bitmap mostrarImagen(String url){
+    private Bitmap mostrarImagen(String url) {
         ImageView imagen;
         MostrarImagen mostrar = new MostrarImagen(url);
         Thread t = new Thread(mostrar);
         t.start();
         try {
             t.join();
-        }catch(Exception e) {
+        } catch (Exception e) {
 
         }
-       return  mostrar.getImagen();
+        return mostrar.getImagen();
     }
 }
