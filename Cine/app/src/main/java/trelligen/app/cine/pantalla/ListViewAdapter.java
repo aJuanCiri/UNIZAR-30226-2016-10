710d72b4fb32e9d9bb85f6ff695328d2949149e0
aguiu <aguiuperez@gmail.com>
2016-05-12 12:18:27 +0200

    Selección de películas en la p. de resultados

--- Cine/app/src/main/java/trelligen/app/cine/pantalla/ListViewAdapter.java
+++ Cine/app/src/main/java/trelligen/app/cine/pantalla/ListViewAdapter.java
@@ -17,58 +17,78 @@ import trelligen.app.cine.R;
 import trelligen.app.cine.objeto.MostrarImagen;
 import trelligen.app.cine.objeto.Pelicula;
 
+/**
+ * Clase que muestra una serie de películas por pantalla.
+ */
 public class ListViewAdapter extends BaseAdapter {
-    // Declare Variables
+
     Context context;
-    Pelicula[][] peliculas;
+    Pelicula[][] peliculas; //Arrat de películas.
     LayoutInflater inflater;
 
+	
+	/*
+	 * Método que obtiene las películas de cada fila.
+     */
     public ListViewAdapter(Context context, ArrayList<Pelicula> listaRecibida) {
         this.context = context;
         peliculas = new Pelicula[(listaRecibida.size()+1)/2][2];
-        int i=0,j=0;
-        while(listaRecibida.size()>j){
+        int i=0,j=0;	// Índices para recorrer el array.
+        while(listaRecibida.size()>j){	// Recorre la lista de películas y las obtiene.
             peliculas[i][0] = listaRecibida.get(j);
-            if(listaRecibida.size()>j+1) {  //Comprueba que haya una segunda pelicula para esta fila
+            if(listaRecibida.size()>j+1) {  //Comprueba que haya una segunda pelicula para esta fila.
                 peliculas[i][1] = listaRecibida.get(j + 1);
             }
             i++; j+=2;
         }
     }
 
+	/*
+	 * Método que devuelve el número de filas de películas.
+     */
     @Override
     public int getCount() {
         return peliculas.length;
     }
 
+	/*
+     * Método que devuelve la película de una posición.
+     */
     @Override
     public Object getItem(int position) {
         return null;
     }
 
+	/*
+     * Método que obtiene el índice del elemento de una posición dada.
+     */
     @Override
     public long getItemId(int position) {
         return 0;
     }
 
+	/*
+     * Método que muestra las películas por pantalla, indicando dos por fila.
+     */
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        // Declare Variables
+        // Variables para las dos películas de cada fila.
         final TextView txtTitle1,txtTitle2;
         ImageView imgImg1,imgImg2;
 
-        //http://developer.android.com/intl/es/reference/android/view/LayoutInflater.html
+        // Prepara la pantalla para mostrar las películas.
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
+		// Número de películas a mostrar.
         View itemView = inflater.inflate(R.layout.list_row, parent, false);
 
-        // Locate the TextViews in listview_item.xml
+        // Obtiene los elementos de la pantalla.
         txtTitle1 = (TextView) itemView.findViewById(R.id.resultado_titulo1);
         imgImg1 = (ImageView) itemView.findViewById(R.id.resultado_imagen1);
         txtTitle2 = (TextView) itemView.findViewById(R.id.resultado_titulo2);
         imgImg2 = (ImageView) itemView.findViewById(R.id.resultado_imagen2);
 
-        // Capture position and set to the TextViews
+        // Comprueba si existen películas para esa posición y las muestra u oculta.
         if(peliculas[position][0]==null){
             imgImg1.setVisibility(View.INVISIBLE);
             txtTitle1.setVisibility(View.INVISIBLE);
@@ -106,6 +126,9 @@ public class ListViewAdapter extends BaseAdapter {
         return itemView;
     }
 
+	/*
+     * Método que muestra las imágenes por pantalla.
+     */
     private Bitmap mostrarImagen(String url){
         ImageView imagen;
         MostrarImagen mostrar = new MostrarImagen(url);
